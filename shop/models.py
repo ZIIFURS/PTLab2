932ded01203599a9661dbc3723a18b1e08e67399
Podkolodnev Artem Vyacheslavovich <ziif616@gmail.com>
2025-11-24 21:46:16 +0300

    Change models, html layouts, settings, views for my individual variant and task. Also add any tests for models and views

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,30 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)  # количество на складе
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity} шт.)"
+
+    def can_be_purchased(self):
+        return self.quantity > 0
+
+    def decrease_quantity(self):
+        if self.quantity <= 0:
+            raise ValidationError("Товара нет в наличии")
+        self.quantity -= 1
+        self.save(update_fields=['quantity'])
+
 
 class Purchase(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     person = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.person} купил {self.product.name}"
--- shop/views.py
+++ shop/views.py
@@ -1,21 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic.edit import CreateView
-
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.http import HttpResponseBadRequest
 from .models import Product, Purchase
 
-# Create your views here.
+
 def index(request):
     products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'shop/index.html', context)
+    return render(request, 'shop/index.html', {'products': products})
 
 
 class PurchaseCreate(CreateView):
     model = Purchase
-    fields = ['product', 'person', 'address']
+    fields = ['person', 'address']
+    template_name = 'shop/purchase_form.html'
+    success_url = reverse_lazy('index')
+
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        self.product = get_object_or_404(Product, id=kwargs['product_id'])
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.product.can_be_purchased():
+            return render(request, 'shop/out_of_stock.html', {
+                'product': self.product
+            }, status=400)
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
-        self.object = form.save()
-        return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
+        purchase = form.save(commit=False)
+        purchase.product = self.product
+        purchase.save()
 
+        # Уменьшаем количество товара
+        self.product.decrease_quantity()
+
+        return render(self.request, 'shop/purchase_success.html', {
+            'person': purchase.person,
+            'product': self.product
+        })
